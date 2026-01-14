f6c35d939d6a8a78c8e2fb11f2caa4c7c5be5211
Hadeel Balasmeh <109764053+Hadeelmb97@users.noreply.github.com>
2026-01-14 21:42:01 +0300

    Add src/utils/AnimatePresence.js

---
+++ src/utils/AnimatePresence.js
@@ -0,0 +1,44 @@
+export const Notifications = ({ messages }) => (
+  <AnimatePresence>
+    {messages.map(({ id, content }) => (
+      <motion.li
+        key={id}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        {content}
+      </motion.li>
+    ))}
+  </AnimatePresence>
+)
+
+export const slideIn = (direction, type, delay, duration) => {
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const staggerContainer = (staggerChildren, delayChildren) => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: staggerChildren,
+        delayChildren: delayChildren || 0,
+      },
+    },
+  };
+};
