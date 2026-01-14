0c209d7acd2bdb166a49228e1634c1069a42b1fc
Hadeel Balasmeh <109764053+Hadeelmb97@users.noreply.github.com>
2026-01-14 21:37:38 +0300

    Add src/akin.jsx

---
+++ src/akin.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  Aboutcopy,
+
+
+} from "./components";
+
+const Akin = () => {
+  return (
+    <div className=" bg primary">
+          < Aboutcopy />  
+    </div>
+  );
+};
+
+export default Akin;
